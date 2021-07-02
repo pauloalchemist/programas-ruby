46123f36732d35ccf6ef5c2d30cf0377570e5b29
pauloalchemist <paulolinsdev@gmail.com>
2021-07-02 15:38:53 -0300

    variáveis e string no Ruby

---
+++ variaveis.rb
@@ -0,0 +1,8 @@
+character_name = 'Paulo'
+character_age = 38
+
+puts "Olá pessoal, meu nome é #{character_name}"
+puts "Tenho #{character_age} anos"
+
+frase = 'Oi, testando variáveis e strings no Ruby'
+puts frase.length
