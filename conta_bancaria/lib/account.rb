acd835ecc1fc351e5e96444b864b7eda8109f783
pauloalchemist <paulolinsdev@gmail.com>
2021-07-28 18:48:56 -0300

    method signatures: tranfer and debit

--- conta_bancaria/lib/account.rb
+++ conta_bancaria/lib/account.rb
@@ -4,9 +4,19 @@ class Account
     @value = init_value
   end
 
-  def tranfer(other_account, value); end
+  def tranfer(_other_account, value)
+    if balance >= value
+
+    else
+      puts 'Não consegui transferir. Estou Liso.'
+    end
+  end
 
   def balance
     @value
+  end
+
+  private def debit(value)
+    @value -= value
   end
 end
