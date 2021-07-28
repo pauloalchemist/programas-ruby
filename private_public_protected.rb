bde16e1612bfd291888b68abe23adde8f3d5ef86
pauloalchemist <paulolinsdev@gmail.com>
2021-07-28 17:00:52 -0300

    método m5 protegido de ser chamado por outro objeto

--- private_public_protected.rb
+++ private_public_protected.rb
@@ -1,18 +1,31 @@
 class MinhaClasse
   def m1
     puts 'm1'
+    m2
+    m3
+    m5
   end
 
-  private def m2
+  def m2
     puts 'm2'
   end
 
   def m3
     puts 'm3'
   end
+
+  private def m5
+    puts 'm5'
+  end
 end
 
-obj = MinhaClasse.new
-obj.m1
-# obj.m2
-obj.m3
+class SubClasse < MinhaClasse
+  def m4
+    m1
+    outro_obj = MinhaClasse.new
+    outro_obj.m5
+  end
+end
+
+obj = SubClasse.new
+obj.m4
