401a41dea668ddd3ba44d84d03490029d20fff9b
pauloalchemist <paulolinsdev@gmail.com>
2021-07-29 21:10:00 -0300

    correção para método transfer

--- conta_bancaria/transfer.rb
+++ conta_bancaria/transfer.rb
@@ -1,14 +1,15 @@
 require_relative './lib/account'
+require_relative './lib/rate_account'
 
-conta_Paulo = Account.new('Paulo', 500)
+conta_Paulo = RateAccount.new('Paulo', 500)
 conta_Vanessa = Account.new('Vanessa', 600)
 
-conta_Paulo.tranfer(conta_Vanessa, 200)
+conta_Paulo.transfer(conta_Vanessa, 200)
 
 puts "Saldo de Paulo é: #{conta_Paulo.balance}"
 puts "Saldo de Vanessa é: #{conta_Vanessa.balance}"
 
-conta_Paulo.tranfer(conta_Vanessa, 400)
+conta_Paulo.transfer(conta_Vanessa, 400)
 
 puts "Saldo de Paulo é: #{conta_Paulo.balance}"
 puts "Saldo de Vanessa é: #{conta_Vanessa.balance}"
