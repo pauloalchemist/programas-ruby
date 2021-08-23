27eb9cb91b34e062118affd48a8d82ff9b15db3e
pauloalchemist <paulolinsdev@gmail.com>
2021-08-23 18:13:15 -0300

    ajuste na pasta

--- RoR_mvc/db/schema.rb
+++ RoR_mvc/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_224901) do
+ActiveRecord::Schema.define(version: 2021_08_14_195308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "value"
+    t.integer "restaurant_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
