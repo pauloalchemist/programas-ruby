30e2b6b958c3d3c90be60e0893d8afd8a2955088
pauloalchemist <paulolinsdev@gmail.com>
2021-09-28 10:57:07 -0300

    pmt rails

---
+++ pmt_rails/app/controllers/loans_controller.rb
@@ -0,0 +1,22 @@
+class LoansController < ApplicationController
+  def create
+    @loan = Loan.new(loan_params)
+
+    if @loan.save
+      render json: { Loan: { id: @loan[:id] } }, status: 201
+    else
+      raise 'Erro ao gerar PMT. Tente novamente.' unless @loan.save
+    end
+  end
+
+  def show
+    @loan = Loan.find(params[:id])
+    render json: { Loan: { id: @loan[:id], pmt: @loan[:pmt] } }, status: 200
+  end
+
+  private
+
+  def loan_params
+    params.require(:loan).permit(:financed_amount, :rate, :months)
+  end
+end
